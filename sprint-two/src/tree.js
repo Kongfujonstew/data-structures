62d5395608a8f9e3e7dd476b245ab6f232988451
Hack Reactor Student <student@hackreactor.com>
2017-03-30 11:34:50 -0700

    Completed Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,39 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var wasFound = false;
+  var helper = function(tree) {
+    //console.log(tree.value);
+    if (tree.value === target) {
+      wasFound = true;
+    }
+    for (var i = 0; i < tree.children.length; i++) {
+      console.log(i);
+      helper(tree.children[i]);
+    }
+  }
+  helper(this);
+  return wasFound;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+ addChild - O(1)
+ contains - O(n)
+
  */
