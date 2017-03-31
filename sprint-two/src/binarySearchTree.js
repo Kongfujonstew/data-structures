bc644e9578add698dfb77fbd7a77462b839e7e01
Hack Reactor Student <student@hackreactor.com>
2017-03-30 19:52:32 -0700

    Completed Set and Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,68 @@
 var BinarySearchTree = function(value) {
+  var newTree = Object.create(BinarySearchTree.prototype);
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  return newTree;
 };
 
+BinarySearchTree.prototype.insert = function(newValue) {
+  if (newValue < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(newValue);
+    } else {
+      this.left.insert(newValue);
+    }
+  } else if (newValue > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(newValue)
+    } else {
+      this.right.insert(newValue);
+    }
+  } else {
+    this.left = BinarySearchTree(newValue);
+  }
+};
+
+BinarySearchTree.prototype.contains = function(val) {
+  var wasFound = false;
+  var searchTree = function(tree) {
+    if (tree.value === val) {
+      wasFound = true;
+    } else if (val < tree.value) {
+      if (tree.left) {
+        searchTree(tree.left);
+      }
+    } else if (val > tree.value) {
+      if (tree.right) {
+        searchTree(tree.right);
+      }
+    }
+  }
+  searchTree(this);
+  return wasFound;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  var traverseTree = function(tree) {
+    if (tree.value) {
+      cb(tree.value);
+    }
+    if (tree.left) {
+      tree.left.depthFirstLog(cb);
+      }
+    if (tree.right) {
+      tree.right.depthFirstLog(cb);
+    }
+  };
+  traverseTree(this);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+insert - O(log(n))
+contains - O(log(n))
+depthFirstLog - O(n)
+
  */
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item] === item) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage[item] === item) {
+    this._storage[item] = undefined;
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+add - O(1)
+contains - O(1)
+remove - O(1)
+
  */
