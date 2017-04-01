6aea946781e92ea09fb5b1fdbb7844c9cb4df526
Hack Reactor Student <student@hackreactor.com>
2017-04-01 09:12:27 -0700

    hashTable refactor

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -19,48 +19,45 @@ HashTable.prototype.insert = function(k, v) {
     }
   }
   if (this._filled >= (this._limit * 0.625)) {
-    this._storage = this.rebuildStorage(this._limit, 'double');
+    this._storage = this.rebuildStorage(2);
     this._limit *= 2;
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  for (var i = 0; i < this._storage.get(index).length; i++) {
-    if (this._storage.get(index)[i][0] === k) {
-      return this._storage.get(index)[i][1];
+  if (this._storage.get(index)) {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        return this._storage.get(index)[i][1];
+      }
     }
   }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage.get(index)){
-    for (var i = 0; i < this._storage.get(index).length; i++) {
-      if (this._storage.get(index)[i][0] === k) {
-        this._storage.get(index)[i][1] = undefined;
-        // this._storage.get(index)[i][0] = undefined;
+  var bucket = this._storage.get(index);
+  if (bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        this._storage.set(index, undefined);
       }
     }
   }
-  if ((this._limit >= 8) && (this._filled < (this._limit * 0.5))) {
-    this._storage = this.rebuildStorage(this._limit, 'half');
+  if ((this._limit >= 8) && (this._filled <= (this._limit * 0.375))) {
+    this._storage = this.rebuildStorage(0.5);
     this._limit /= 2;
   }
 };
-HashTable.prototype.rebuildStorage = function(oldLimit, doubleOrHalve){
+HashTable.prototype.rebuildStorage = function(multiplyStorageSize) {
   var rebuiltHashTable = new HashTable;
-  if (doubleOrHalve === 'double') {
-    rebuiltHashTable._limit = oldLimit * 2;
-  } else if (doubleOrHalve === 'half') {
-    rebuiltHashTable._limit = oldLimit / 2;
-  }
+  rebuiltHashTable._limit = this._limit * multiplyStorageSize;
   rebuiltHashTable._storage = LimitedArray(rebuiltHashTable._limit);
-  for (var i = 0; i < oldLimit; i++) {
+  for (var i = 0; i < this._limit; i++) {
     if (this._storage.get(i)) {
       for (var j = 0; j < this._storage.get(i).length; j++) {
-        rebuiltHashTable.insert(this._storage.get(i)[j][0], 
-          this._storage.get(i)[j][1]);
+        rebuiltHashTable.insert(this._storage.get(i)[j][0], this._storage.get(i)[j][1]);
       }
     }
   }
