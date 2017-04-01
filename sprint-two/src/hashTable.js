3fb88df3e3acd74b886cf0052444d5fbb2dc3ce7
Hack Reactor Student <student@hackreactor.com>
2017-03-31 21:49:26 -0700

    Fixed formatting

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,62 +1,82 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._filled = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (!this._storage[index]) {
-    this._storage[index] = [[k, v]];
+  if (!this._storage.get(index)) {
+    this._storage.set(index, [[k, v]]);
+    this._filled++;
   } else if (!this.retrieve(k)) {
-    this._storage[index].push([k, v]);
+    this._storage.get(index).push([k, v]);
   } else {
-    this._storage[index].forEach(function(tuple) {
-      if (tuple[0] === k) {
-        tuple[1] = v;
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        this._storage.get(index)[i][1] = v;
       }
-    });
+    }
+  }
+  console.log(this._filled);
+  if (this._filled >= (this._limit * .625)) {
+    this._limit = this._limit * 2;
+    // this._storage = rebuildStorage(this, this._limit);
+    // rebuildHashTable(this, this._limit);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var result;
-  this._storage[index].forEach(function(tuple) {
-    if (tuple[0] === k) {
-      result = tuple[1];
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      return this._storage.get(index)[i][1];
     }
-  });
-  return result;
-  // for (var i = 0; i < this._storage[index].length; i++) {
-  //   if (this._storage[index][i][0] === k) {
-  //     return this._storage[index][i][1];
-  //   }
-  // }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  console.log(this._storage[index]);
-  this._storage[index].forEach(function(tuple, index, col) {
-    if (tuple[0] === k) {
-      console.log('before', tuple);
-      // col[index] = undefined;
-      //tuple = undefined;
-       //tuple.pop();
-      // tuple.pop();
-      tuple[1] = undefined;
-      tuple[0] = undefined;
-      console.log(tuple);
-    }
-  });
-  // for (var i = 0; i < this._storage[index].length; i++) {
-  //   if (this._storage[index][i][0] === k) {
-  //     this._storage[index][i][0] = undefined;
-  //     this._storage[index][i][1] = undefined;
-  //   }
-  // }
   // this.insert(k, undefined);
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      this._storage.get(index)[i][1] = undefined;
+      this._storage.get(index)[i][0] = undefined;
+    }
+  }
+  // if (this._limit !== 8  && (this._filled < (this._limit * .25))) {
+  //   console.log('half');
+  //   this._limit = this._limit / 2;
+  // }
 };
+
+var rebuildStorage = function(oldHashTable, newLimit){
+  var rebuiltHashTable = new HashTable;
+  for (var i = 0; i < newLimit; i++) {
+    if (oldHashTable._storage.get(i)) {
+      for (var j = 0; j < oldHashTable._storage.get(i).length; j++) {
+        var key = oldHashTable._storage.get(i)[j][0];
+        var value = oldHashTable._storage.get(i)[j][1];
+        rebuiltHashTable.insert(key, value);
+      }
+    }
+  }
+  return rebuiltHashTable._storage;
+};
+// var rebuildHashTable = function(oldHashTable, oldLimit){
+//   var rebuiltHashTable = new HashTable;
+//   rebuiltHashTable._limit = oldLimit * 2;
+//   for (var i = 0; i < oldLimit; i++) {
+//     if (oldHashTable._storage.get(i)) {
+//       for (var j = 0; j < oldHashTable._storage.get(i).length; j++) {
+//         var key = oldHashTable._storage.get(i)[j][0];
+//         var value = oldHashTable._storage.get(i)[j][1];
+//         rebuiltHashTable.insert(key, value);
+//       }
+//     }
+//   }
+//   return rebuiltHashTable;
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
